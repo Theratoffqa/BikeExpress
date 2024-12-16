200c77758c321a7b7089d4c423ed40509902266e
Theratoffqa <pedro.barazorda@unmsm.edu.pe>
2024-12-15 23:15:52 -0500

    Agregando dependencias y barra lateral

--- app/src/main/java/unmsm/ux/bikeexpress/mapa.kt
+++ app/src/main/java/unmsm/ux/bikeexpress/mapa.kt
@@ -2,17 +2,19 @@ package unmsm.ux.bikeexpress
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.material.navigation.NavigationView
 
 
 class mapa : AppCompatActivity(),  OnMapReadyCallback {
@@ -38,6 +40,32 @@ class mapa : AppCompatActivity(),  OnMapReadyCallback {
         imageButtonqr.setOnClickListener {
             val intent = Intent(this, qrActivity::class.java)
             startActivity(intent)
+        }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+
+        // Configura el botón "rayas" para abrir el Drawer
+        val imageButtonRayas = findViewById<ImageButton>(R.id.imageButton4)
+        imageButtonRayas.setOnClickListener {
+            // Abre el DrawerLayout cuando se hace clic en "rayas"
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Configura el Listener para el NavigationView
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    // Maneja la acción de "Home"
+                }
+                /*
+                R.id.nav_settings -> {
+                    // Maneja la acción de "Settings"
+                }*/
+            }
+            // Cierra el Drawer después de seleccionar una opción
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
         }
 
 
